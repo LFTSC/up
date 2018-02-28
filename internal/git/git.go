1219dfb635bb11f36920175bde6b8542e87b87c5
TJ Holowaychuk <tj@vision-media.ca>
2018-02-28 19:06:18 +0000

    misc

--- internal/git/git.go
+++ internal/git/git.go
@@ -22,7 +22,9 @@ func IsRepo(dir string) bool {
 		return false
 	}
 
-	cmd := exec.Command(bin, "-C", dir, "status")
+	cmd := exec.Command(bin, "status")
+	cmd.Dir = dir
+
 	if err := cmd.Run(); err != nil {
 		return false
 	}
@@ -37,7 +39,9 @@ func Describe(dir string) (string, error) {
 		return "", errors.Wrap(err, "looking up git")
 	}
 
-	cmd := exec.Command(bin, "-C", dir, "describe", "--abbrev=0", "--dirty=DIRTY")
+	cmd := exec.Command(bin, "describe", "--abbrev=0", "--dirty=DIRTY")
+	cmd.Dir = dir
+
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", errors.Wrap(err, "executing")
