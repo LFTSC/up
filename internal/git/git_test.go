d10170b64a7a1cab0859e72f110968cf445b7605
TJ Holowaychuk <tj@vision-media.ca>
2018-02-28 19:29:15 +0000

    misc

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -11,3 +11,9 @@ func TestIsRepo(t *testing.T) {
 	assert.True(t, git.IsRepo("."))
 	assert.False(t, git.IsRepo("/tmp"))
 }
+
+func TestDescribe(t *testing.T) {
+	s, err := git.Describe("/tmp")
+	assert.EqualError(t, err, `git repo not found`)
+	assert.Empty(t, s)
+}
