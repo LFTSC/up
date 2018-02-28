cb92e32dbc52655b338d20fa0d73b5ebd3ff846d
TJ Holowaychuk <tj@vision-media.ca>
2018-02-28 19:06:45 +0000

    misc

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -1,8 +1,6 @@
 package git_test
 
 import (
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/apex/up/internal/git"
@@ -12,11 +10,4 @@ import (
 func TestIsRepo(t *testing.T) {
 	assert.True(t, git.IsRepo("."))
 	assert.False(t, git.IsRepo("/tmp"))
-}
-
-func TestDescribe(t *testing.T) {
-	s, err := git.Describe(filepath.Join("..", ".."))
-	assert.NoError(t, err)
-	assert.NotEmpty(t, s)
-	assert.True(t, strings.HasPrefix(s, "v"), "should have 'v' prefix")
 }
