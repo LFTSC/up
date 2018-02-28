d8f923103c283dc9554764d768b1f734655e4545
TJ Holowaychuk <tj@vision-media.ca>
2018-02-28 19:00:03 +0000

    misc

---
+++ internal/git/git_test.go
@@ -0,0 +1,17 @@
+package git_test
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/apex/up/internal/git"
+	"github.com/tj/assert"
+)
+
+func TestDescribe(t *testing.T) {
+	s, err := git.Describe(filepath.Join("..", ".."))
+	assert.NoError(t, err)
+	assert.NotEmpty(t, s)
+	assert.True(t, strings.HasPrefix(s, "v"), "should have 'v' prefix")
+}
